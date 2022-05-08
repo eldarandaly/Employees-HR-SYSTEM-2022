cf06cf4dc17e849afe9d6a7d899c636035e93aa3
Ahmed Mamdouh Eldarandaly <ahmedmamdouh430@gmail.com>
2022-05-08 21:38:09 +0200

    guiUpdate

--- GUIPy/ui_newemployee.py
+++ GUIPy/ui_newemployee.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'newemployeeTVbMwo.ui'
+## Form generated from reading UI file 'newemployeelZKuAa.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -45,13 +45,13 @@ class Ui_newemp1(object):
 "	color: rgb(0,0,0);\n"
 "	padding: 2px;\n"
 "	background-color: rgb(186,186,186);\n"
-"    background-image: url(\"D:/ahmos/Desktop/senior proj 222/APP/icon1.jpg\");\n"
+"  \n"
 "}\n"
 "QPushButton:hover{\n"
 "	border-style: solid;\n"
-"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(195, 195, 195), stop:1 rgb(222, 222, 222));"
-                        "\n"
-"	border-right-color: qlineargradient(spread:pad, x1:0, y1:0.5, x2:1, y2:0.5, stop:0 rgb(197, 197, 197), stop:1 rgb(227, 227, 227));\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(195, 195, 195), stop:1 rgb(222, 222, 222));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0, y1:0.5, x2:1, y2"
+                        ":0.5, stop:0 rgb(197, 197, 197), stop:1 rgb(227, 227, 227));\n"
 "	border-left-color: qlineargradient(spread:pad, x1:0, y1:0.5, x2:1, y2:0.5, stop:0 rgb(227, 227, 227), stop:1 rgb(197, 197, 197));\n"
 "	border-bottom-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(195, 195, 195), stop:1 rgb(222, 222, 222));\n"
 "	border-width: 1px;\n"
@@ -65,8 +65,8 @@ class Ui_newemp1(object):
 "	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(215, 215, 215), stop:1 rgb(222, 222, 222));\n"
 "	border-right-color: qlineargradient(spread:pad, x1:0, y1:0.5, x2:1, y2:0.5, stop:0 rgb(217, 217, 217), stop:1 rgb(227, 227, 227));\n"
 "	border-left-color: qlineargradient(spread:pad, x1:0, y1:0.5, x2:1, y2:0.5, stop:0 rgb(227, 227, 227), stop:1 rgb(217, 217, 217));\n"
-"	border-bottom-co"
-                        "lor: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(215, 215, 215), stop:1 rgb(222, 222, 222));\n"
+"	border-bottom-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgb(215,"
+                        " 215, 215), stop:1 rgb(222, 222, 222));\n"
 "	border-width: 1px;\n"
 "	border-radius: 5px;\n"
 "	color: rgb(0,0,0);\n"
